9d82fe43d3079e442e594156bc0f4f5d6e111ddb
sgo <sgo@thinkerit.be>
2016-08-03 18:06:11 +0200

    Wait till application gateway is initialised.

--- src/binarta-gateways.js
+++ src/binarta-gateways.js
@@ -1,19 +1,34 @@
 (function () {
+    angular.module('binarta-applicationjs-gateways-angular1', ['binarta-applicationjs-rest-angular1'])
+        .provider('binartaApplicationGateway', ['restBinartaApplicationGatewayProvider', proxy]);
+
     angular.module('binarta-checkpointjs-gateways-angular1', ['binarta-checkpointjs-rest-angular1'])
         .provider('binartaCheckpointGateway', ['restBinartaCheckpointGatewayProvider', proxy]);
 
     angular.module('binarta-shopjs-gateways-angular1', ['binarta-shopjs-rest-angular1'])
         .provider('binartaShopGateway', ['restBinartaShopGatewayProvider', proxy]);
 
-    angular.module('binarta-gateways-angular1', ['binartajs-angular1', 'binarta-checkpointjs-gateways-angular1', 'binarta-shopjs-gateways-angular1'])
-        .run(['$q', 'binartaGatewaysAreInitialised', 'binartaCheckpointGatewayIsInitialised', 'binartaShopGatewayIsInitialised', IsInitialised]);
+    angular.module('binarta-gateways-angular1', [
+        'binartajs-angular1',
+        'binarta-applicationjs-gateways-angular1',
+        'binarta-checkpointjs-gateways-angular1',
+        'binarta-shopjs-gateways-angular1'
+    ])
+        .run([
+            '$q',
+            'binartaGatewaysAreInitialised',
+            'binartaApplicationGatewayIsInitialised',
+            'binartaCheckpointGatewayIsInitialised',
+            'binartaShopGatewayIsInitialised',
+            IsInitialised
+        ]);
 
     function proxy(gateway) {
         return gateway;
     }
 
-    function IsInitialised($q, gatewaysAreInitialised, checkpointGateway, shopGateway) {
-        $q.all(checkpointGateway.promise, shopGateway.promise).then(function() {
+    function IsInitialised($q, gatewaysAreInitialised, applicationGateway, checkpointGateway, shopGateway) {
+        $q.all(applicationGateway.promise, checkpointGateway.promise, shopGateway.promise).then(function() {
             gatewaysAreInitialised.resolve();
         });
     }
