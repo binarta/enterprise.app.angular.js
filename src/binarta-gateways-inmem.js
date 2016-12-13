99827703839c027b67aef9904073a201565caea3
Jo Kerkhofs <jke@thinkerit.be>
2016-12-13 10:47:58 +0100

    Added implementation for inmem wiring of dependencies for use in e2e context

---
+++ src/binarta-gateways-inmem.js
@@ -0,0 +1,31 @@
+(function () {
+    angular.module('binarta-applicationjs-gateways-angular1', ['binarta-applicationjs-inmem-angular1'])
+        .provider('binartaApplicationGateway', ['inmemBinartaApplicationGatewayProvider', proxy]);
+
+    angular.module('binarta-checkpointjs-gateways-angular1', ['binarta-checkpointjs-inmem-angular1'])
+        .provider('binartaCheckpointGateway', ['inmemBinartaCheckpointGatewayProvider', proxy]);
+
+    angular.module('binarta-shopjs-gateways-angular1', ['binarta-shopjs-inmem-angular1'])
+        .provider('binartaShopGateway', ['inmemBinartaShopGatewayProvider', proxy]);
+
+    angular.module('binarta-gateways-angular1', [
+        'binartajs-angular1',
+        'binarta-applicationjs-gateways-angular1',
+        'binarta-checkpointjs-gateways-angular1',
+        'binarta-shopjs-gateways-angular1'
+    ])
+        .run(['binartaGatewaysAreInitialised', 'binartaApplicationGateway', IsInitialised]);
+
+    function proxy(gateway) {
+        return gateway;
+    }
+
+    function IsInitialised(gatewaysAreInitialised, applicationGateway) {
+        applicationGateway.addPublicConfig({
+            id: 'application.pages.contact.active',
+            value: 'true'
+        });
+
+        gatewaysAreInitialised.resolve();
+    }
+})();
