b60050ea439709c790d5f08364871c74d9aea5bc
sgo <sgo@thinkerit.be>
2018-08-27 16:21:18 +0200

    Installing publisher gateway.

--- src/binarta-gateways.js
+++ src/binarta-gateways.js
@@ -5,6 +5,9 @@
     angular.module('binarta-checkpointjs-gateways-angular1', ['binarta-checkpointjs-rest-angular1'])
         .provider('binartaCheckpointGateway', ['restBinartaCheckpointGatewayProvider', proxy]);
 
+    angular.module('binarta-publisherjs-gateways-angular1', ['binarta-publisherjs-rest-angular1'])
+        .provider('binartaPublisherGateway', ['restBinartaPublisherGatewayProvider', proxy]);
+
     angular.module('binarta-shopjs-gateways-angular1', ['binarta-shopjs-rest-angular1'])
         .provider('binartaShopGateway', ['restBinartaShopGatewayProvider', proxy]);
 
@@ -15,6 +18,7 @@
         'binartajs-angular1',
         'binarta-applicationjs-gateways-angular1',
         'binarta-checkpointjs-gateways-angular1',
+        'binarta-publisherjs-gateways-angular1',
         'binarta-shopjs-gateways-angular1',
         'binarta-humanresourcesjs-gateways-angular1'
     ])
@@ -23,6 +27,7 @@
             'binartaGatewaysAreInitialised',
             'binartaApplicationGatewayIsInitialised',
             'binartaCheckpointGatewayIsInitialised',
+            'binartaPublisherGatewayIsInitialised',
             'binartaShopGatewayIsInitialised',
             'binartaHumanResourcesGatewayIsInitialised',
             IsInitialised
@@ -32,8 +37,8 @@
         return gateway;
     }
 
-    function IsInitialised($q, gatewaysAreInitialised, applicationGateway, checkpointGateway, shopGateway, humanResourcesGateway) {
-        $q.all(applicationGateway.promise, checkpointGateway.promise, shopGateway.promise, humanResourcesGateway.promise).then(function() {
+    function IsInitialised($q, gatewaysAreInitialised, applicationGateway, checkpointGateway, publisherGateway, shopGateway, humanResourcesGateway) {
+        $q.all(applicationGateway.promise, checkpointGateway.promise, publisherGateway.promise, shopGateway.promise, humanResourcesGateway.promise).then(function() {
             gatewaysAreInitialised.resolve();
         });
     }
